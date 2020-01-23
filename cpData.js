ffbc0cdff7f9ffc30f432d2a1f0b7df0b7c98432
mcGrid <antnf3@gmail.com>
2020-01-23 17:20:37 +0900

    Update

--- cpData.js
+++ cpData.js
@@ -1,4 +1,5 @@
 const arrJson =[
 {href: "https://coupa.ng/bmZQUN", src: "https://static.coupangcdn.com/image/affiliate/banner/c4de26970855d446aa05d2a9a143f137@2x.jpg", alt: "태광선식 옛날 11곡 미숫가루, 2kg, 1개", width: "120", height: "240", date: "202001231713"},
-{href: "https://coupa.ng/bmZUr4", src: "https://static.coupangcdn.com/image/affiliate/banner/d3234f537b7f2ddff1a21f0cb677304c@2x.jpg", alt: "청정원 카레여왕 인도식마크니커리, 100g, 1개", width: "120", height: "240", date: "202001231713"}
+{href: "https://coupa.ng/bmZUr4", src: "https://static.coupangcdn.com/image/affiliate/banner/d3234f537b7f2ddff1a21f0cb677304c@2x.jpg", alt: "청정원 카레여왕 인도식마크니커리, 100g, 1개", width: "120", height: "240", date: "202001231713"},
+{href: "https://coupa.ng/bm4mxe", src: "https://static.coupangcdn.com/image/affiliate/banner/f895d8c26a786acac2e495f20f75f3f5@2x.jpg", alt: "동원샘물 미네마인, 2L, 12개", width: "120", height: "240", date: "202001231718"}
 ];
