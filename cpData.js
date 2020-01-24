1470412f4e49761ead7e024428024aaefb064800
mcGrid <antnf3@gmail.com>
2020-01-24 21:10:10 +0900

    update

--- cpData.js
+++ cpData.js
@@ -1,5 +1,7 @@
 const arrJson =[
 {href: "https://coupa.ng/bmZQUN", src: "https://static.coupangcdn.com/image/affiliate/banner/c4de26970855d446aa05d2a9a143f137@2x.jpg", alt: "태광선식 옛날 11곡 미숫가루, 2kg, 1개", width: "120", height: "240", date: "202001231713"},
 {href: "https://coupa.ng/bmZUr4", src: "https://static.coupangcdn.com/image/affiliate/banner/d3234f537b7f2ddff1a21f0cb677304c@2x.jpg", alt: "청정원 카레여왕 인도식마크니커리, 100g, 1개", width: "120", height: "240", date: "202001231713"},
-{href: "https://coupa.ng/bm4mxe", src: "https://static.coupangcdn.com/image/affiliate/banner/f895d8c26a786acac2e495f20f75f3f5@2x.jpg", alt: "동원샘물 미네마인, 2L, 12개", width: "120", height: "240", date: "202001231718"}
+{href: "https://coupa.ng/bm4mxe", src: "https://static.coupangcdn.com/image/affiliate/banner/f895d8c26a786acac2e495f20f75f3f5@2x.jpg", alt: "동원샘물 미네마인, 2L, 12개", width: "120", height: "240", date: "202001231718"},
+{href: "https://coupa.ng/bm9QpI", src: "https://static.coupangcdn.com/image/affiliate/banner/793ab456bed98e4146de0117ce9d65b7@2x.jpg", alt: "[사전예약] PS4 그랑블루 판타지 버서스 한글 일반판", width: "120", height: "240", date: "202001242107"},
+{href: "https://coupa.ng/bm9Qwk", src: "https://static.coupangcdn.com/image/affiliate/banner/a4124f205c13cb4661d2fd0157f3d71c@2x.jpg", alt: "[사전예약] LG전자 2020년 그램17 노트북 17ZD90N-VX5BK (10세대 i5-1035G7 43.1cm WIN미포함), 미포함, SSD 256GB, 8GB", width: "120", height: "240", date: "202001242107"}
 ];
