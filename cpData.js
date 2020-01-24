0079684345675d86da1f6ffc441b07fd0db9ea9b
mcGrid <antnf3@gmail.com>
2020-01-25 07:26:27 +0900

    update

--- cpData.js
+++ cpData.js
@@ -3,5 +3,6 @@ const arrJson =[
 {href: "https://coupa.ng/bmZUr4", src: "https://static.coupangcdn.com/image/affiliate/banner/d3234f537b7f2ddff1a21f0cb677304c@2x.jpg", alt: "청정원 카레여왕 인도식마크니커리, 100g, 1개", width: "120", height: "240", date: "202001231713"},
 {href: "https://coupa.ng/bm4mxe", src: "https://static.coupangcdn.com/image/affiliate/banner/f895d8c26a786acac2e495f20f75f3f5@2x.jpg", alt: "동원샘물 미네마인, 2L, 12개", width: "120", height: "240", date: "202001231718"},
 {href: "https://coupa.ng/bm9QpI", src: "https://static.coupangcdn.com/image/affiliate/banner/793ab456bed98e4146de0117ce9d65b7@2x.jpg", alt: "[사전예약] PS4 그랑블루 판타지 버서스 한글 일반판", width: "120", height: "240", date: "202001242107"},
-{href: "https://coupa.ng/bm9Qwk", src: "https://static.coupangcdn.com/image/affiliate/banner/a4124f205c13cb4661d2fd0157f3d71c@2x.jpg", alt: "[사전예약] LG전자 2020년 그램17 노트북 17ZD90N-VX5BK (10세대 i5-1035G7 43.1cm WIN미포함), 미포함, SSD 256GB, 8GB", width: "120", height: "240", date: "202001242107"}
+{href: "https://coupa.ng/bm9Qwk", src: "https://static.coupangcdn.com/image/affiliate/banner/a4124f205c13cb4661d2fd0157f3d71c@2x.jpg", alt: "[사전예약] LG전자 2020년 그램17 노트북 17ZD90N-VX5BK (10세대 i5-1035G7 43.1cm WIN미포함), 미포함, SSD 256GB, 8GB", width: "120", height: "240", date: "202001242107"},
+{href: "https://coupa.ng/bnbG1g", src: "https://static.coupangcdn.com/image/affiliate/banner/73783fd425d487e26a7bd8d323aa2cda@2x.jpg", alt: "베베숲 저자극 센시티브 엠보싱 물티슈 캡형, 80매, 10팩", width: "120", height: "240", date: "202001250725"}
 ];
