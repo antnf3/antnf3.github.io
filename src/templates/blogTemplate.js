5a69d3812507638caae95841d60eeb175df3ac76
antnf3 <antnf3@gmail.com>
2020-04-22 17:41:19 +0900

    기본정리

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,13 +1,13 @@
-import React from "react"
-import Helmet from 'react-helmet';
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+  const { markdownRemark } = data; // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark;
   return (
     <Layout>
       <Helmet>
@@ -16,7 +16,6 @@ export default function Template({
       </Helmet>
       <div className="blog-post-container">
         <article className="post">
-          
           {!frontmatter.thumbnail && (
             <div className="post-thumbnail">
               <h1 className="post-title">{frontmatter.title}</h1>
@@ -24,10 +23,16 @@ export default function Template({
             </div>
           )}
           {!!frontmatter.thumbnail && (
-            <div className="post-thumbnail" style={{backgroundImage: `url(${frontmatter.thumbnail})`}}>
+            <>
+              <div
+                className="post-thumbnail"
+                style={{ backgroundImage: `url(${frontmatter.thumbnail})` }}
+              >
+                {/* <h1 className="post-title">{frontmatter.title}</h1>
+              <div className="post-meta">{frontmatter.date}</div> */}
+              </div>
               <h1 className="post-title">{frontmatter.title}</h1>
-              <div className="post-meta">{frontmatter.date}</div>
-            </div>
+            </>
           )}
           <div
             className="blog-post-content"
@@ -36,7 +41,7 @@ export default function Template({
         </article>
       </div>
     </Layout>
-  )
+  );
 }
 
 export const pageQuery = graphql`
@@ -52,4 +57,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
